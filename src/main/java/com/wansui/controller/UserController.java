31302989c283bcb32c9b9e21a8260acd44d9f6de
wansui <wanjinhang@gmail.com>
2023-10-13 17:32:21 +0800

    用户登录需求第一次提交

---
+++ src/main/java/com/wansui/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.wansui.controller;
+
+import com.wansui.pojo.User;
+import com.wansui.service.UserService;
+import com.wansui.util.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author wansui
+ * @date 2023/10/13
+ */
+@RestController
+@RequestMapping("user")
+@CrossOrigin
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @PostMapping("login")
+    public Result login(@RequestBody User user) {
+        UserService.login(user);
+        return  null;
+    }
+}
--- src/main/java/com/wansui/service/impl/UserServiceImpl.java
+++ src/main/java/com/wansui/service/impl/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package com.wansui.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.wansui.pojo.User;
 import com.wansui.service.UserService;
 import com.wansui.mapper.UserMapper;
+import com.wansui.util.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,6 +17,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     implements UserService{
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public Result login(User user){
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<User>();
+        LambdaQueryWrapper<User> eq = wrapper.eq(User::getUsername, user.getUsername());
+        User user1 = userMapper.selectOne(eq);
+
+
+        return null;
+    }
 
 }
 
